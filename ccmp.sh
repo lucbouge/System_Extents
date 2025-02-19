63cf78cf7c468bcd3f6aae8559287f6785acb135
Luc Bougé <Luc.Bouge@ens-rennes.fr>
2025-02-19 17:41:31 +0100

    Work

--- ccmp.sh
+++ ccmp.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env /bin/bash
+set -u
 
 # Clone-aware cmp
 
 GETOPT_PATH='/usr/local/opt/gnu-getopt/bin/getopt'
 
-if [ $(uname) = Darwin ]
+if [[ "$(uname)" = Darwin ]]
 then
   if [[ ! -x "${GETOPT_PATH}" ]]
   then 
@@ -13,6 +14,8 @@ then
   fi
   export PATH="$(dirname "GETOPT_PATH"):$PATH" # use GNU getopt
 fi
+
+################################################################################
 
 function version() { 
   echo ccmp v1.0 Aug 2021
@@ -41,7 +44,7 @@ function usage() {
 
 ################################################################################
 
-CMP='/usr/bin/cmp'
+export REGULAR_CMP='/usr/bin/cmp'
 
 OUT_FILE="/tmp/cmpout$$"
 ERR_FILE="/tmp/cmperr$$"
@@ -49,9 +52,11 @@ ERR_FILE="/tmp/cmperr$$"
 trap 'rm -rf ${OUT_FILE} ${ERR_FILE}' 0
 trap exit 2 15
 
-declare -a ORIGINAL_ARGS
-declare -a CMP_OPTIONS
-declare -a EXTRA_ARGS
+declare -a ORIGINAL_ARGS=()
+declare -a CMP_OPTIONS=()
+declare -a EXTRA_ARGS=()
+declare -a EXTENTS_ARGS=()
+
 
 AWK1_="'"'{$5+='"'"'"$START1"'"'"'; $5 = $5 ","; sub(/, line [0-9]*/,""); print}'"'" # for stdout
 AWK2="'"'/EOF/ {$7+='"'"'"$START1"'"'"'; $7 = $7 ","; sub(/, in line [0-9]*/,""); print; next}'"'" #stderr
@@ -60,9 +65,23 @@ declare -i EXIT_ON_FIRST_DIFFERENCE=1 # exit on first difference
 declare -i BYTES=0
 declare -i SKIP=0
 declare -i VERBOSE=0
+declare -i GLOBAL_CCMP_STATUS=0
+
+declare AWK1=""
+declare AWK2=""
 
 ORIGINAL_ARGS=("$@")
-ARGS=$(getopt -n "$0" -l "help,ignore-initial:,print_bytes,verbose,bytes:,quiet,silent,version" -o "bhi:ln:sv" -- "$@")
+PROGRAM_NAME="$0"
+
+ARGS=$(\
+  getopt -n "${PROGRAM_NAME}" \
+  -l "help,ignore-initial:,print_bytes,verbose,bytes:,quiet,silent,version" \
+  -o "bhi:ln:sv" \
+  -- "$@" \
+  )
+
+echo $ARGS
+
 if [ $? -ne 0 ]
 then
     usage
@@ -70,9 +89,11 @@ then
 fi
 
 eval set -- "$ARGS"
-while :
+
+while ((1))
 do
     case "$1" in
+      ##
       '-b'|'--print-bytes')
         BYTES=1 
         ONE=1
@@ -80,17 +101,17 @@ do
         AWK1="'"'{$5+='"'"'"$START1"'"'"'; printf "%s %s %s %s %s %s %3s %s %3s %s\n", $1, $2, $3, $4, $5, $8, $9, $10, $11, $12 }'"'"
 	      #AWK2="'{print}'"
         shift;;
-
+      ##
       '-h'|'--help') 
         help
         exit 0;;
-
+      ##
       '-i'|'--ignore-initial')
 	      SKIP=1
 	      EXTRA_ARGS+=('-i' "$2")
         AWK1="'"'{$1-='"'"'"$START1"'"'"'; printf "%7s %3s %3s\n", $1, $2, $3}'"'"
 	      shift 2;;
-
+      ##
       '-l'|'--verbose')
         VERBOSE=1 
         STOP_AT_FIRST_ERROR=0
@@ -99,30 +120,32 @@ do
 #      	AWK2="'"'/EOF/ {$7+='"'"'"$START1"'"'"'; sub(/which is/,"after byte");print}'"'"
       	AWK2="'"'/EOF/ {$7+='"'"'"$START1"'"'"'; print}'"'"
         shift;;
-
+      ##
       '-n'|'--bytes')
         #CMPOPTS+=(-n "$2")
         EXTRA_ARGS+=('-n' "$2")
         shift 2;;
-      
+      ##
       '-s'|'--quiet'|'--silent') 
         AWK1='{}'
         AWK2='{}'
         shift;;
-
+      ##
       '-v'|'--version') 
         version
         exit;;
-
+      ##
       '--') 
         shift; 
         break;;
-
+      ##
       *) 
         echo 'arg parsing error'
         exit 1;;
     esac
 done
+
+################################################################################
 
 if (( BYTES && VERBOSE ))
 then
@@ -138,77 +161,93 @@ then
     fi
 fi
 
+################################################################################
+
 case $# in
     1) 
-      ${CMP} "${ORIGINAL_[@]}"
+      ${CCMP} "${ORIGINAL_ARGS[@]}"
       exit $?
       ;;
-
+    ##
     2) 
       A="$1"
       B="$2"
       ;;
-
+    ##
     3) 
       A="$1"
       B="$2" 
       EXTRA_ARGS+=('-i' "$3")
       ;;
-    
+    ##
     4) 
       A="$1" 
       B="$2" 
       EXTRA_ARGS+=('-i' "$3:$4") 
       ;;
-
+    ##
     *) 
       usage
       exit 
       ;;
 esac
 
-function CMP_AWK_LOOP {
-  declare -i START1 START2 LENGTH CMP
-  # GLOBAL_CMP_STATUS is the logical AND of all the inverted cmp statuses 
+
+################################################################################
+
+function CCMP_AWK_LOOP {
+  declare -i START1 START2 LENGTH CCMP
+  CCMP=
+  # GLOBAL_CCMP_STATUS is the logical AND of all the inverted cmp statuses 
   # (ie 1 if all returned 1, 0 otherwise)
-  declare -i GLOBAL_CMP_STATUS=1
+  declare -i GLOBAL_CCMP_STATUS=1
   ##
   while read START1 START2 LENGTH
   do
-      ${CMP} -i "$START1:$START2" -n "$LENGTH" "$A" "$B" >${OUT_FILE} 2>${ERR_FILE}
-      CMP_RETURN_VALUE=$?
+      ${CCMP} -i "$START1:$START2" -n "$LENGTH" "$A" "$B" >${OUT_FILE} 2>${ERR_FILE}
+      CCMP_RETURN_VALUE=$?
       ##
       eval awk "$AWK1" "${OUT_FILE}"
       eval awk "$AWK2" "${ERR_FILE}" 1>&2
       ##
-      # cmp returns 0 for same, 1 for different
-      (( GLOBAL_CMP_STATUS= GLOBAL_CMP_STATUS && ! CMP_RETURN_VALUE )) 
-      if (( EXIT_ON_FIRST_DIFFERENCE && CMP_RETURN_VALUE ))
+      # CCMP returns 0 for same, 1 for different
+      (( GLOBAL_CCMP_STATUS= GLOBAL_CCMP_STATUS && ! CCMP_RETURN_VALUE )) 
+      if (( EXIT_ON_FIRST_DIFFERENCE && CCMP_RETURN_VALUE ))
       then 
         break
       fi
   done
-  return ${GLOBAL_CMP_STATUS}
+  return ${GLOBAL_CCMP_STATUS}
 }
+
+################################################################################
 
 ## extents can be fooled by write data in flight, so stabilize - use fsync in extents?
 # sync
 
-CMP_DIR="$(dirname "$0")"
-PATH="${CMP_DIR}:$PATH"
+CCMP_DIR="$(dirname "$0")"
+PATH="${CCMP_DIR}:$PATH"
 
-(set -x; extents -c "${EXTRA_ARGS[@]}" "$A" "$B") | CMP_AWK_LOOP
+if [[ "${#EXTRA_ARGS}" = 0 ]]
+then
+  EXTENTS_ARGS=("$A" "$B")
+else
+  EXTENTS_ARGS=${EXTRA_ARGS}
+  EXTENTS_ARGS+=("$A" "$B")
+fi
+
+(set -x; extents -c "${EXTENTS_ARGS[@]}") | CCMP_AWK_LOOP
 
 declare -a PIPE_STATUS=("${PIPESTATUS[@]}")
-CMP_AWK_LOOP_EXIT_STATUS=${PIPE_STATUS[1]}
+CCMP_AWK_LOOP_EXIT_STATUS=${PIPE_STATUS[1]}
 EXTENTS_EXIT_STATUS=${PIPE_STATUS[0]}
 
 
 if (( EXTENTS_EXIT_STATUS != 0 ))
-then # extents failed, fall back to cmp
-    ${CMP} ${ORIGINAL_ARGS[@]}
-    GLOBAL_CMP_STATUS=$?
+then # extents failed, fall back to regular cmp
+    ${REGULAR_CMP} ${ORIGINAL_ARGS[@]}
+    GLOBAL_CCMP_STATUS=$?
 else
-    (( GLOBAL_CMP_STATUS = ! GLOBAL_CMP_STATUS ))
+    (( GLOBAL_CCMP_STATUS = ! GLOBAL_CCMP_STATUS ))
 fi
-exit $GLOBAL_CMP_STATUS
+exit ${GLOBAL_CCMP_STATUS}
