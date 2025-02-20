0edbab047be3b114ca0346b6e50cc5c19051ff11
Luc Bougé <Luc.Bouge@ens-rennes.fr>
2025-02-20 14:41:43 +0100

    Work

---
+++ ccmp_original.sh
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+
+# Clone-aware cmp
+
+if [ $(uname) = Darwin ]
+then
+  if [ ! -x /usr/local/opt/gnu-getopt/bin/getopt ]
+  then echo "must install gnu getopt" ; exit 1
+  fi
+  PATH="/usr/local/opt/gnu-getopt/bin:$PATH" # use gnu getopt
+fi
+
+version() { echo ccmp v1.0 Aug 2021; }
+
+help() {
+    usage
+    echo
+    version
+    echo
+    cat <<\xxx
+Acts like cmp(1), but uses "extents" (which must be in the same dir or on the $PATH)
+to apply cmp only to blocks that are not shared between file1 and file2.
+<opts> are the same as those for cmp.  -b -l -s are the only useful ones.
+Setting a skip or limit will only result in incorrect output.
+
+Author: Mario Wolczko mario@wolczko.com
+
+See LICENSE file for licensing.
+xxx
+}
+
+usage() { echo usage: "$0" '<cmp-opts> file1 file2' ; }
+
+trap 'rm -f /tmp/cmpout$$ /tmp/cmperr$$' 0
+trap exit 2 15
+
+declare -a OrigArgs=("$*")
+declare -a CMPOPTS
+declare -a ExtArgs
+AWK1="'"'{$5+='"'"'"$Start1"'"'"'; $5 = $5 ","; sub(/, line [0-9]*/,""); print}'"'" # for stdout
+AWK2="'"'/EOF/ {$7+='"'"'"$Start1"'"'"'; $7 = $7 ","; sub(/, in line [0-9]*/,""); print; next}'"'" #stderr
+ONE=1 # exit on first difference
+BYTES=0
+SKIP=0
+VERBOSE=0
+ARGS=$(getopt -n "$0" -l "help,ignore-initial:,print_bytes,verbose,bytes:,quiet,silent,version" -o "bhi:ln:sv" -- "$@")
+if [ $? -ne 0 ]; then
+    usage; exit;
+fi
+
+eval set -- "$ARGS"
+while :
+do
+    case "$1" in
+      -b|--print-bytes)
+        BYTES=1  ONE=1
+        CMPOPTS+=(-b)
+        AWK1="'"'{$5+='"'"'"$Start1"'"'"'; printf "%s %s %s %s %s %s %3s %s %3s %s\n", $1, $2, $3, $4, $5, $8, $9, $10, $11, $12 }'"'"
+	      #AWK2="'{print}'"
+        shift;;
+      -h|--help) help; exit 0;;
+      -i|--ignore-initial)
+	      SKIP=1
+	      #CMPOPTS+=(-i "$2")
+	      ExtArgs+=(-i "$2")
+        AWK1="'"'{$1-='"'"'"$Start1"'"'"'; printf "%7s %3s %3s\n", $1, $2, $3}'"'"
+	      shift 2;;
+      -l|--verbose)
+        VERBOSE=1 ONE=0
+        CMPOPTS+=(-l)
+        AWK1="'"'{$1+='"'"'"$Start1"'"'"'; printf "%7s %3s %3s\n", $1, $2, $3}'"'"
+#      	AWK2="'"'/EOF/ {$7+='"'"'"$Start1"'"'"'; sub(/which is/,"after byte");print}'"'"
+      	AWK2="'"'/EOF/ {$7+='"'"'"$Start1"'"'"'; print}'"'"
+        shift;;
+      -n|--bytes)
+        #CMPOPTS+=(-n "$2")
+        ExtArgs+=(-n "$2")
+        shift 2;;
+      -s|--quiet|silent) AWK1="'{}'"; AWK2="$AWK1"; shift;;
+      -v|--version) version; exit;;
+      --) shift; break;;
+      *) echo 'arg parsing error'; exit 1;;
+    esac
+done
+
+if (( BYTES && VERBOSE ))
+then
+    if (( ! SKIP ))
+    then
+	    AWK1="'"'{$1+='"'"'"$Start1"'"'"'; printf "%7s %3s %-4s %3s %s\n", $1, $2, $3, $4, $5}'"'"
+#	    AWK2="'"'/EOF/ {$7+='"'"'"$Start1"'"'"'; sub(/which is/,"after byte"); print}'"'"
+    	    AWK2="'"'/EOF/ {$7+='"'"'"$Start1"'"'"'; print}'"'"
+    else
+	    AWK1="'"'{printf "%s %3s %-4s %3s %s\n", $1, $2, $3, $4, $5}'"'"
+#	    AWK2="'"'/EOF/ {sub(/which is/,"after byte"); print}'"'"
+    	    AWK2="'"'/EOF/ {print}'"'"
+    fi
+    
+fi
+
+case $# in
+    1) cmp "${OrigArgs[@]}" ; exit $? ;;
+    2) A="$1" B="$2" ;;
+    3) A="$1" B="$2" ExtArgs+=(-i "$3") ;;
+    4) A="$1" B="$2" ExtArgs+=(-i "$3:$4") ;;
+    *) usage ; exit ;;
+esac
+
+cmpAwkLoop() {
+  declare -i Start1 Start2 Len Cmp
+  declare -i Cmps=1 # logical AND of all the inverted cmp statuses (ie 1 if all returned 1 [ie same], 0 otherwise)
+  while read Start1 Start2 Len
+  do
+      cmp "${CMPOPTS[@]}" -i "$Start1:$Start2" -n "$Len" "$A" "$B" >/tmp/cmpout$$ 2>/tmp/cmperr$$
+      Cmp=$?
+      eval awk "$AWK1" /tmp/cmpout$$
+      eval awk "$AWK2" /tmp/cmperr$$ 1>&2
+      (( Cmps = Cmps && ! Cmp )) # cmp returns 0 for same, 1 for different
+      if (( ONE && Cmp ))
+      then break
+      fi
+  done
+  return "$Cmps"
+}
+
+#sync # extents can be fooled by write data in flight, so stabilize - use fsync in extents? XXX
+PATH=$(dirname "$0")":$PATH"
+extents -c "${ExtArgs[@]}" "$A" "$B" | cmpAwkLoop
+declare -a SAVED_PS=("${PIPESTATUS[@]}")
+Cmps=${SAVED_PS[1]}
+if [ ${SAVED_PS[0]} -ne 0 ]
+then # extents failed, fall back to cmp
+    cmp ${OrigArgs[@]} ; Cmps=$?
+else
+    Cmps=$(( ! Cmps ))
+fi
+exit $Cmps
---
+++ test_ccmp.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash 
+
+set -u 
+
+trap 'eval rm -f ${T}? /tmp/golden*$$ /tmp/output$$ /tmp/err$$ /tmp/opts$$ /tmp/pairs$$ $TESTS ${T}-self-?-?.dat' 0
+trap exit 2 15
+
+# put the files with shared extents here-- must be in a filesystem that supports reflinks
+TEST_DIR=${TEST_DIR:-/tmp}
+SUFFIX="$$"
+T="$TEST_DIR/ccmp_test_file_${SUFFIX}_"
+
+CMP='/usr/bin/cmp'
+CCMP='./ccmp.sh'
+
+TEST_FILE="/tmp/ccmp_test_${SUFFIX}"
+OPT_FILE="/tmp/ccmp_options_${SUFFIX}"
+PAIR_FILE="/tmp/ccmp_pairs_${SUFFIX}"
+GOLDEN_OUT_FILE="/tmp/ccmp_golden_out_${SUFFIX}"
+GOLDEN_ERR_FILE="/tmp/ccmp_golden_err_${SUFFIX}"
+CCMP_OUT_FILE="/tmp/ccmp_out_${SUFFIX}"
+CCMP_ERR_FILE="/tmp/ccmp_err_${SUFFIX}"
+
+# make a file
+dd if=/dev/random of="${T}0" count=2048 2>/dev/null
+
+case $(uname) in
+Linux)  CP="cp --reflink" ;;
+Darwin) CP="cp -c" ;;
+esac
+
+copy() {
+    $CP "$@" ||
+	  { echo "$CP failed -- perhaps filesystem at $TEST_DIR does not support reflinks?"; exit 1; }
+}
+
+# Clone it a few times
+copy "${T}0" "${T}1" # same
+copy "${T}0" "${T}2" ; echo foo | dd "of=${T}2" bs=1 seek=409600 conv=notrunc 2>/dev/null   # one change
+copy "${T}2" "${T}3" ; echo bar | dd "of=${T}3" bs=1 seek=942080 conv=notrunc 2>/dev/null   # two changes from ${T}0
+copy "${T}0" "${T}4" ; echo -n foo >> "${T}4"  # longer
+copy "${T}0" "${T}5" ; echo -n bar >> "${T}5"  # longer but different
+copy "${T}0" "${T}6" ; echo -n barf >> "${T}6" # longest
+
+sync # needed to ensure data are stable in allocated file extents
+
+# compare and check output against cmp
+
+declare -a CCMP_OPTIONS=('-b' '-l' '-s' '-bl')
+
+declare -a CCMP_PAIRS=(
+"${T}0:${T}1"
+"${T}0:${T}2"
+"${T}0:${T}3"
+"${T}3:${T}4"
+"${T}4:${T}5"
+"${T}5:${T}6"
+)
+
+declare -a TESTS_CASES=()
+for OPTION in "${CCMP_OPTIONS[@]}"
+do
+    for PAIR in "${CCMP_PAIRS[@]}"
+    do 
+        FILES="${PAIR/:/ }"
+	    TESTS_CASES+="$OPTION $FILES"
+    done 
+done
+
+TEST_CASES+=(
+"-bl -i 409600:1048576 ${T}2 ${T}4"
+"-bl -i 409600:1048576 ${T}3 ${T}5"
+"-bl -i 409600:1048576 ${T}3 ${T}6"
+)
+
+declare -i FAILED GOOD
+FAILED=0
+GOOD=1
+
+start() {
+    echo -n "Comparing: ${TEST_ARGS}"
+    GOOD=1
+}
+    
+fail() {
+    GOOD=0 FAILED=1
+    echo "Failed: $1"
+}
+
+faildiff() {
+    fail "$3"
+    echo diff cmp vs ccmp ; diff "$1" "$2"
+    echo
+}    
+
+for TEST_ARGS in "${TEST_CASES[@]}"
+do
+    printf "TEST_ARGS: %s" "$TEST_ARGS"
+    rm -f "${GOLDEN_OUT_FILE}" "${GOLDEN_ERR_FILE}" "${CCMP_OUT_FILE}" "${CCMP_ERR_FILE}"
+    ##
+    ${CMP} $TEST_ARGS > "${GOLDEN_OUT_FILE}" 2> "${GOLDEN_ERR_FILE}"
+    CMP_EXIT_STATUS=$?
+    ##
+    ${CCMP} $TEST_ARGS > "${CCMP_OUT_FILE}" 2> "${CCMP_ERR_FILE}"
+    CCMP_EXIT_STATUS=$?
+    ##
+    if [[ ${CCMP_EXIT_STATUS} != ${CMP_EXIT_STATUS} ]]
+    then fail "exit status differs (${CMP_EXIT_STATUS} for cmp, ${CCMP_EXIT_STATUS} for ccmp)" 
+    fi
+    ##
+    if ! cmp -s "${GOLDEN_OUT_FILE}"  "${CCMP_OUT_FILE}"
+    then faildiff "${GOLDEN_OUT_FILE}" "${CCMP_OUT_FILE}" "stdout differs" 
+    fi
+    if ! cmp -s "${GOLDEN_ERR_FILE}" "${CCMP_ERR_FILE}"
+    then faildiff "${GOLDEN_ERR_FILE}" "${CCMP_ERR_FILE}" "stderr differs" 
+    fi
+    if [[ $GOOD -eq 1 ]]
+    then echo Passed
+    fi
+done
+
+exit $FAILED
